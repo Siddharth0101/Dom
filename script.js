0376b7d36290ffe688d45055e5c91aee7fef45e9
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2023-10-11 16:20:05 +0530

    Dom index

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GetElemetsById
+// let headerTitle = document.getElementById("header-title");
+// let header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "hi";
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = "<h3>hello</h3>";
+// header.style.borderBottom = "solid 3px #000";
